d08ee1a20e67385ca3589b6409ee87b3be42733e
Jose Manuel Ganchozo <jganchozo@outlook.com>
2024-08-28 11:45:00 -0400

    Commands Added

---
+++ CQRS-ES/CQRS.Core/Messages/Message.cs
@@ -0,0 +1,8 @@
+namespace CQRS.Core.Messages
+{
+    public abstract class Message
+    {
+        public Guid Id { get; set; }
+
+    }
+}
---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/NewPostCommand.cs
@@ -0,0 +1,8 @@
+using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands;
+public class NewPostCommand : BaseCommand
+{
+    public string Author { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
