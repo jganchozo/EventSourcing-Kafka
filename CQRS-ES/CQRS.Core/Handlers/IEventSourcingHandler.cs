6a6d6b20b06f500341701cf6100d5602663910c6
Jose Manuel Ganchozo <jganchozo@outlook.com>
2024-10-24 15:59:35 -0400

    ICommandHandler and CommandHandler Added

--- CQRS-ES/CQRS.Core/Handlers/IEventSourcingHandler.cs
+++ CQRS-ES/CQRS.Core/Handlers/IEventSourcingHandler.cs
@@ -4,4 +4,5 @@ namespace CQRS.Core.Handlers;
 public interface IEventSourcingHandler<T>
 {
     Task SaveAsync(AggregateRoot aggregate);
-    Task<T> GetByIdAsync(Guid aggreg
+    Task<T> GetByIdAsync(Guid aggregateId);
+}
